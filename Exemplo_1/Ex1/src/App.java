c047a29225d109218d5f706cbd8fa3e220f51e32
“LucasFranczak” <lucasfranczak@gmail.com>
2024-01-02 21:11:59 -0300

    01/01

--- Exemplo_1/Ex1/src/App.java
+++ Exemplo_1/Ex1/src/App.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String args[]) {
         System.out.println("Hello, World!");
@@ -10,10 +16,61 @@ public class App {
         System.out.println(cliente.getCodigo());
         System.out.println(cliente.getEndereço());
         
+        // solicitar um valor
+        Scanner s = new Scanner(System.in);
+         System.out.println("informe valor");
+         int valor=s.nextInt();
+
+        // laços de repetição
+        // If - Else (se for verdadeiro executa)
+        int result = 0;
+
+		if (result > 1) {
+			// Cod
+		} else {
+			//cod
+		}
+
+        // For (para;condiçao,encremento)
+        for(int i = 0; i < 10; i++) {
+            //cod
+        }
+
+        // while (enquanto)
+        int count = 0;
+        while(count < 2) {
+          // cod
+          count++;
+        }
+
+        // do while
+        do {
+            //cod
+            count++;
+       } while (count < 2);
+
+       //Switch, Case e Default
+       //O switch verifica uma variável e age de acordo com os seus casos. Exemplo.
+        SWITCH (variável) {
+            CASE valor :
+                   //cod Código a ser executado caso o valor de case seja o mesmo da variável de switch.
+	            break;
+            CASE valor:
+	              //outro código;
+	            break;
+        }
+
+        //LISTAS\\
+        //ArrayList: É como um array cujo tamanho pode crescer. A busca de um elemento é rápida, mas inserções e exclusões não são.
+        List<String> lista =new ArrayList<String>();
+        lista.add("Nome");
+
+        // para ordenar uma lista
+        Collections.sort(lista);
 
 
     }
-}
+}   
 
 
 // set - adicionar valor 
--- Exemplo_1/Ex1/src/Cliente.java
+++ Exemplo_1/Ex1/src/Cliente.java
@@ -6,7 +6,7 @@ public class Cliente {
     private String nome;
     private String endereco;
 
-
+    
     public int getCodigo() {
         return codigo;
     }
